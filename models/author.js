3f4fe72b94e6b7993893f1f3e1297dfabd6bb5e9
kacor11 <kubakacor@gmail.com>
2020-06-22 17:15:46 +0200

    first version of library app with node/express

---
+++ models/author.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, max: 100},
+    family_name: {type: String, required: true, max: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+AuthorSchema
+.virtual('name')
+.get(function() {
+  let fullname = '';
+  if(this.first_name && this.family_name) {
+    fullname = this.family_name + ', ' + this.first_name;
+    return fullname;
+  }
+  if(this.first_name || this.family_name) {
+    fullname = '';
+    return fullname;
+  }
+});
+
+AuthorSchema
+.virtual('lifespan')
+.get(function() {
+  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+});
+
+AuthorSchema
+.virtual('url')
+.get(function() {
+  return '/catalog/author/' + this._id;
+})
+
+AuthorSchema
+.virtual('momentBirthDate')
+.get(function() {
+  return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+})
+
+AuthorSchema
+.virtual('momentDeathDate')
+.get(function() {
+  return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
+})
+module.exports = mongoose.model('Author', AuthorSchema);
